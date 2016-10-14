61566038e693d28c36cd7325799c874b5f604d78
John Fewell <fewell@gmail.com>
2016-10-14 19:28:15 +0000

    Building out methods and menu

--- lib/darksky_cli_app/cli.rb
+++ lib/darksky_cli_app/cli.rb
@@ -1,17 +1,30 @@
 class DarkskyCliApp::CLI
 
   def call
-    puts "Hey you!"
-    todays_weather
     menu
   end
 
+  def todays_weather(location)
+    #the scrapping will happen here
+    puts "Today's weather in #{location} is 68 degrees"
+  end
 
-  def todays_weather
-    puts "Today's weather in Brooklyn is 68 degrees"
+  def ten_day_forecast
+    #this will scrape the 10 day forecast
+  end
+
+  def get_geo(location)
+    #this will use the geolocation gem
+    location
   end
 
   def menu
+    input = nil
+    while input != "exit"
+    puts "Enter a location to get today's weather forcast:"
+    input = gets.strip.downcase
+    if check_location(input) == nil
+      puts "Could not find that location, please try another"
     puts "Do you want the 10 day forcast for this location or do ytou want to find today's weather at a different location?"
   end
 
