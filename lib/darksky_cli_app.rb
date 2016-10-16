21304f9cac30c258b175b4f9c83cda362436d219
John Fewell <fewell@gmail.com>
2016-10-16 19:54:07 +0000

    Weather class working better

--- lib/darksky_cli_app.rb
+++ lib/darksky_cli_app.rb
@@ -2,6 +2,10 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative "./darksky_cli_app/version"
-require_relative './darksky_cli_app/cli'
-require_relative './darksky_cli_app/weather'
+module DarkskyCliApp
+
+  require_relative "darksky_cli_app/version"
+  require_relative 'darksky_cli_app/cli'
+  require_relative 'darksky_cli_app/weather'
+
+end
--- lib/darksky_cli_app/weather.rb
+++ lib/darksky_cli_app/weather.rb
@@ -1,24 +1,43 @@
 class DarkskyCliApp::Weather
+attr_accessor :name, :now, :later, :now_desc, :hr1, :hr2, :hr3, :hr4, :hr5, :hr6, :hr7, :hr8, :hr9, :hr10, :hr11, :hr12
 
   def today(location)
+    weather_here = self
+    weather_here.name = location
+    weather_here.now = "#{weather_here.name}: 62˚ Clear"
+    weather_here.now_desc = "Clear throughout the day."
+    weather_here.later = "Next Hour: Clear. No precipitation anywhere in the area."
+    weather_here.hr1 = "Now: 62˚"
+    weather_here.hr2 = "6pm: 61˚"
+    weather_here.hr3 = "8pm: 57˚"
+    weather_here.hr4 = "10pm: 54˚"
+    weather_here.hr5 = "12am: 52˚"
+    weather_here.hr6 = "2am: 50˚"
+    weather_here.hr7 = "4am: 49˚"
+    weather_here.hr8 = "6am: 47˚"
+    weather_here.hr9 = "8am: 48˚"
+    weather_here.hr10 = "10am: 55˚"
+    weather_here.hr11 = "12pm: 60˚"
+    weather_here.hr12 = "2pm: 64˚"
+
     puts ""
     puts "----------------------"
-    puts "#{location}: 62˚ Clear"
-    puts "Clear throughout the day."
-    puts "Next Hour: Clear. No precipitation anywhere in the area."
+    puts weather_here.now
+    puts weather_here.now_desc
+    puts weather_here.later
     puts "----------------------"
-    puts "Now: 62˚"
-    puts "6pm: 61˚"
-    puts "8pm: 57˚"
-    puts "10pm: 54˚"
-    puts "12am: 52˚"
-    puts "2am: 50˚"
-    puts "4am: 49˚"
-    puts "6am: 47˚"
-    puts "8am: 48˚"
-    puts "10am: 55˚"
-    puts "12pm: 60˚"
-    puts "2pm: 64˚"
+    puts weather_here.hr1
+    puts weather_here.hr2
+    puts weather_here.hr3
+    puts weather_here.hr4
+    puts weather_here.hr5
+    puts weather_here.hr6
+    puts weather_here.hr7
+    puts weather_here.hr8
+    puts weather_here.hr9
+    puts weather_here.hr10
+    puts weather_here.hr11
+    puts weather_here.hr12
     puts "----------------------"
     puts ""
   end
