f54b6ff6449a0a78d3ab6bbdfe5a46f3cf3add37
John Fewell <fewell@gmail.com>
2017-01-13 05:59:24 +0000

    Refactoring weather to make fewer http requests

--- lib/darksky_cli_app/weather.rb
+++ lib/darksky_cli_app/weather.rb
@@ -1,48 +1,53 @@
 require 'pry'
 class DarkskyCliApp::Weather
-attr_accessor :name, :now, :later, :later_desc, :now_one_word, :days_temps_max, :days_names, :days_summary, :days_temps_min, :url
+attr_accessor :name, :url, :later, :later_desc, :now_one_word, :days_names, :days_temps_max, :days_temps_min, :days_summary
 
-  def initialize(name = nil, url = nil)
+  def initialize(name, url)
     @name = name
     @url = url
-    @now = now
-    @later = later
     @days_temps_max = []
     @days_temps_min = []
     @days_summary = []
     @days_names = []
+    get_page(url)
+  end
+
+#gets the page from the web
+  def get_page(url)
+    @doc = Nokogiri::HTML(open(url))
   end
 
 #this is just the number and degree symbol
   def now_temp
-    @now_temp = doc.xpath("//span[@class='temp swip']").text
+    @now_temp = @doc.xpath("//span[@class='temp swip']").text
   end
 
 #this is the one word desc of the weather
   def now_one_word
-    @now_one_word = doc.xpath("//span[@class='summary swap']").text
+    @now_one_word = @doc.xpath("//span[@class='summary swap']").text
   end
 
 #current weather sentance desc
   def later_desc
-    @later_desc = doc.xpath("//span[@class='next swap']").text.strip
+    @later_desc = @doc.xpath("//span[@class='next swap']").text.strip
   end
 
 #later today weather one sentance
   def later
-    @later = doc.xpath("//div[@id='nextHourClear']").text.strip
+    @later = @doc.xpath("//div[@id='nextHourClear']").text.strip
   end
 
   def days_temps
-    @days_temps_max = doc.xpath("//span[@class='maxTemp']").text.split(/\u02DA/).map! {|x| x + "\u02DA"}
-    @days_temps_min = doc.xpath("//span[@class='minTemp']").text.split(/\u02DA/).map! {|x| x + "\u02DA"}
-    @days_names = doc.xpath("//span[@class='name']").text.split(" ")
-    @days_summary = doc.xpath("//div[@class='summary']").text.strip.gsub! 'in.', 'inches'
+    @days_temps_max = @doc.xpath("//span[@class='maxTemp']").text.split(/\u02DA/).map! {|x| x + "\u02DA"}
+    @days_temps_min = @doc.xpath("//span[@class='minTemp']").text.split(/\u02DA/).map! {|x| x + "\u02DA"}
+    @days_names = @doc.xpath("//span[@class='name']").text.split(" ")
+    @days_summary = @doc.xpath("//div[@class='summary']").text.strip.gsub! 'in.', 'inches'
     @days_summary = @days_summary.split(".").map! {|x| x.strip + "."}
   end
 
-  def doc
-    @doc = Nokogiri::HTML(open(url))
-  end
+
+  # def doc
+  #   @doc
+  # end
 
 end
