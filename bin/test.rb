902727bbecccc03e9d93cb3d05edcd2491141458
John Fewell <fewell@gmail.com>
2017-01-14 15:18:58 -0500

    Delete test.rb

--- bin/test.rb
+++
@@ -1,9 +0,0 @@
-require "rubygems"
-require "open-uri"
-require "nokogiri"
-class scrape
-		page = Nokogiri::HTML(open("https://www.darksky.net/40.7306,-73.9866")
-		file = File.open("scrape.html", 'w')
-		file.write(page)
-		file.close
-end
